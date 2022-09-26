a249af996c6592a5db0a0dfbabb0cb44639f337f
Alessandro Amici <a.amici@bopen.eu>
2022-09-26 17:57:29 +0200

    Allow selecting the integration-tests URL via a environment variable

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
+import os
+
 import pytest
 
 
 @pytest.fixture
 def dev_env_api_url() -> str:
-    return "http://localhost:8080/api"
+    return os.environ.get("CADS_API_ROOT_URL", "http://localhost:8080/api")
