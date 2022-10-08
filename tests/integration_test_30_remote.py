b004db9e871ffd6c85232433011f4f129faf0cc5
Alessandro Amici <a.amici@bopen.eu>
2022-10-08 16:13:17 +0200

    Add retrieve with the CAMS adaptor to the integration tests

--- tests/integration_test_30_remote.py
+++ tests/integration_test_30_remote.py
@@ -34,7 +34,9 @@ def test_collection_submit(dev_env_api_url: str) -> None:
     assert isinstance(res.status, str)
 
 
-def test_collection_retrieve(dev_env_api_url: str, tmpdir: py.path.local) -> None:
+def test_collection_retrieve_with_cds_adaptor(
+    dev_env_api_url: str, tmpdir: py.path.local
+) -> None:
     collection_id = "reanalysis-era5-pressure-levels"
     cat = catalogue.Catalogue(f"{dev_env_api_url}/catalogue")
     dataset = cat.collection(collection_id)
@@ -48,6 +50,27 @@ def test_collection_retrieve(dev_env_api_url: str, tmpdir: py.path.local) -> Non
         day="02",
         time="00:00",
         level="1000",
+        target=target,
+        retry_options={"maximum_tries": 0},
+    )
+
+    assert isinstance(res, str)
+    assert res.endswith(target)
+
+
+def test_collection_retrieve_with_cams_adaptor(
+    dev_env_api_url: str, tmpdir: py.path.local
+) -> None:
+    collection_id = "cams-global-reanalysis-eac4-monthly"
+    cat = catalogue.Catalogue(f"{dev_env_api_url}/catalogue")
+    dataset = cat.collection(collection_id)
+    target = str(tmpdir.join("eac4.grib"))
+
+    res = dataset.retrieve(
+        variable="particulate_matter_10um",
+        year="2016",
+        month="02",
+        product_type="monthly_mean",
         target=target,
         retry_options={"maximum_tries": 0},
     )
