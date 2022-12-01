d77664b63b493ec456efe781e7d8756182b2dd73
Alessandro Amici <a.amici@bopen.eu>
2022-12-01 13:06:06 +0100

    Add test for url adapter and configure request

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,3 +12,8 @@ def api_root_url() -> str:
 def api_key() -> str:
     # default to anonymous access
     return os.environ.get("CADS_API_KEY", "00112233-4455-6677-c899-aabbccddeeff")
+
+
+@pytest.fixture
+def request_year() -> str:
+    return os.environ.get("CADS_YEAR", "2016")
--- tests/integration_test_30_remote.py
+++ tests/integration_test_30_remote.py
@@ -13,7 +13,7 @@ def test_from_collection_to_process(api_root_url: str) -> None:
     assert isinstance(res, processing.Process)
 
 
-def test_collection_submit(api_root_url: str, api_key: str) -> None:
+def test_collection_submit(api_root_url: str, api_key: str, request_year: str) -> None:
     collection_id = "reanalysis-era5-pressure-levels"
     headers = {"PRIVATE-TOKEN": api_key}
     cat = catalogue.Catalogue(f"{api_root_url}/catalogue", headers=headers)
@@ -22,7 +22,7 @@ def test_collection_submit(api_root_url: str, api_key: str) -> None:
     res = dataset.submit(
         product_type="reanalysis",
         variable="temperature",
-        year="2022",
+        year=request_year,
         month="01",
         day="01",
         time="00:00",
@@ -36,7 +36,7 @@ def test_collection_submit(api_root_url: str, api_key: str) -> None:
 
 
 def test_collection_retrieve_with_cds_adaptor(
-    api_root_url: str, api_key: str, tmpdir: py.path.local
+    api_root_url: str, api_key: str, request_year: str, tmpdir: py.path.local
 ) -> None:
     collection_id = "reanalysis-era5-pressure-levels"
     headers = {"PRIVATE-TOKEN": api_key}
@@ -47,7 +47,7 @@ def test_collection_retrieve_with_cds_adaptor(
     res = dataset.retrieve(
         product_type="reanalysis",
         variable="temperature",
-        year="2022",
+        year=request_year,
         month="01",
         day="02",
         time="00:00",
@@ -61,7 +61,7 @@ def test_collection_retrieve_with_cds_adaptor(
 
 
 def test_collection_retrieve_with_cams_adaptor(
-    api_root_url: str, api_key: str, tmpdir: py.path.local
+    api_root_url: str, api_key: str, request_year: str, tmpdir: py.path.local
 ) -> None:
     collection_id = "cams-global-reanalysis-eac4-monthly"
     headers = {"PRIVATE-TOKEN": api_key}
@@ -71,9 +71,32 @@ def test_collection_retrieve_with_cams_adaptor(
 
     res = dataset.retrieve(
         variable="particulate_matter_10um",
-        year="2016",
+        year=request_year,
         month="02",
         product_type="monthly_mean",
+        target=target,
+        retry_options={"maximum_tries": 0},
+    )
+
+    assert isinstance(res, str)
+    assert res.endswith(target)
+
+
+def test_collection_retrieve_with_url_adaptor(
+    api_root_url: str, api_key: str, request_year: str, tmpdir: py.path.local
+) -> None:
+    collection_id = "derived-near-surface-meteorological-variables"
+    headers = {"PRIVATE-TOKEN": api_key}
+    cat = catalogue.Catalogue(f"{api_root_url}/catalogue", headers=headers)
+    dataset = cat.collection(collection_id)
+    target = str(tmpdir.join("wfde.zip"))
+
+    res = dataset.retrieve(
+        variable="surface_downwelling_longwave_radiation",
+        reference_dataset="cru",
+        year=request_year,
+        month="04",
+        version="2.1",
         target=target,
         retry_options={"maximum_tries": 0},
     )
