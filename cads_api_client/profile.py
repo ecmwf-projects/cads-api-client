9baf8161d6aa0e5e05985ae513de0cf59044ca07
barghini <a.barghini@bopen.eu>
2022-12-22 10:40:19 +0100

    draft profile not working

---
+++ cads_api_client/profile.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict, List, Optional
+
+import attrs
+
+from . import processing
+
+
+class Profile:
+    supported_api_version = "v1"
+
+    def __init__(
+        self, url: str, headers: Dict[str, Any] = {}
+    ) -> None:
+        self.url = f"{url}/{self.supported_api_version}"
+        self.headers = headers
+
+    def profile(self):
+        url = f"{self.url}/account"
+        response = processing.ApiResponse.from_request("get", url, headers=self.headers)
+        return response.json
+
+    def accept_licence(self, licence_id):
+        url = f"{self.url}/account/licences/{licence_id}"
+        response = processing.ApiResponse.from_request("put", url, headers=self.headers)
+        return response.json
+
+    def accepted_licences(self):
+        url = f"{self.url}/account/licences"
+        print(self.headers)
+        response = processing.ApiResponse.from_request("get", url, headers=self.headers)
+        return response.json
+
+    def update_profile(self):
+        raise NotImplemented
+
+    def access_token(self):
+        raise NotImplemented
+
