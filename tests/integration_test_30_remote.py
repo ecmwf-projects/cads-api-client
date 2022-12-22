aec58d3ee9be638c06f13139cde7eed59fd7c9f1
barghini <a.barghini@bopen.eu>
2022-12-22 17:14:32 +0100

    add contextual licences: not working

--- tests/integration_test_30_remote.py
+++ tests/integration_test_30_remote.py
@@ -16,10 +16,12 @@ def test_from_collection_to_process(api_root_url: str) -> None:
 def test_collection_submit(api_root_url: str, api_key: str, request_year: str) -> None:
     collection_id = "reanalysis-era5-pressure-levels"
     headers = {"PRIVATE-TOKEN": api_key}
+    accepted_licences = [{"id": "licence-to-use-copernicus-products", "revision": 12}]
     cat = catalogue.Catalogue(f"{api_root_url}/catalogue", headers=headers)
     dataset = cat.collection(collection_id)
 
     res = dataset.submit(
+        accepted_licences=accepted_licences,
         product_type="reanalysis",
         variable="temperature",
         year=request_year,
@@ -40,11 +42,14 @@ def test_collection_retrieve_with_cds_adaptor(
 ) -> None:
     collection_id = "reanalysis-era5-pressure-levels"
     headers = {"PRIVATE-TOKEN": api_key}
+    accepted_licences = [{"id": "licence-to-use-copernicus-products", "revision": 12}]
+
     cat = catalogue.Catalogue(f"{api_root_url}/catalogue", headers=headers)
     dataset = cat.collection(collection_id)
     target = str(tmpdir.join("era5.grib"))
 
     res = dataset.retrieve(
+        accepted_licences=accepted_licences,
         product_type="reanalysis",
         variable="temperature",
         year=request_year,
@@ -65,11 +70,14 @@ def test_collection_retrieve_with_ads_adaptor(
 ) -> None:
     collection_id = "cams-global-reanalysis-eac4-monthly"
     headers = {"PRIVATE-TOKEN": api_key}
+    accepted_licences = [{"id": "licence-to-use-copernicus-products", "revision": 12}]
+
     cat = catalogue.Catalogue(f"{api_root_url}/catalogue", headers=headers)
     dataset = cat.collection(collection_id)
     target = str(tmpdir.join("eac4.grib"))
 
     res = dataset.retrieve(
+        accepted_licences=accepted_licences,
         variable="particulate_matter_10um",
         year=request_year,
         month="02",
@@ -103,3 +111,43 @@ def test_collection_retrieve_with_url_adaptor(
 
     assert isinstance(res, str)
     assert res.endswith(target)
+
+
+def test_jobs_list(api_root_url: str, api_key: str, request_year: str) -> None:
+
+    collection_id = "reanalysis-era5-pressure-levels"
+    headers = {"PRIVATE-TOKEN": api_key}
+    accepted_licences = [{"id": "licence-to-use-copernicus-products", "revision": 12}]
+    proc = processing.Processing(f"{api_root_url}/retrieve", headers=headers)
+    process = proc.process(collection_id)
+
+    _ = process.execute(
+        accepted_licences=accepted_licences,
+        inputs=dict(
+            product_type="reanalysis",
+            variable="temperature",
+            year=request_year,
+            month="01",
+            day="01",
+            time="00:00",
+            level="1000",
+        )
+    )
+    _ = process.execute(
+        accepted_licences=accepted_licences,
+        inputs=dict(
+            product_type="reanalysis",
+            variable="temperature",
+            year=request_year,
+            month="02",
+            day="01",
+            time="00:00",
+            level="1000",
+        )
+    )
+
+    res = proc.jobs().json
+    assert len(res["jobs"]) > 2
+
+    res = proc.jobs(params={"limit": 1}).json
+    assert len(res["jobs"]) == 1
