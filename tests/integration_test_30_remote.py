dfe4b1c2b2e1e5acdff31d1428c19bc9c73c406d
Alessandro Amici <a.amici@bopen.eu>
2023-03-22 19:41:39 +0100

    No need to ignore types

--- tests/integration_test_30_remote.py
+++ tests/integration_test_30_remote.py
@@ -147,7 +147,7 @@ def test_collection_retrieve_with_url_adaptor(
     assert res.endswith(target)
 
 
-@pytest.mark.xfail  # type: ignore
+@pytest.mark.xfail
 def test_collection_retrieve_with_direct_mars_adaptor(
     api_root_url: str, api_key: str, request_year: str, tmpdir: py.path.local
 ) -> None:
