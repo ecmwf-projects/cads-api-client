a77905db1da5040ec93b07a81b0cf580a9ebf6af
Marco Cucchi <m.cucchi@bopen.eu>
2023-11-10 17:51:39 +0100

    use anon user for test_collection_missing_licence

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,11 @@ def api_key() -> str:
 
 
 @pytest.fixture
+def api_key_anon() -> str:
+    return "00112233-4455-6677-c899-aabbccddeeff"
+
+
+@pytest.fixture
 def request_year() -> str:
     return os.environ.get("CADS_TEST_YEAR", "2016")
 
