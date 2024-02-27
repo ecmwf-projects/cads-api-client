1c91d7bd3190c32462c29905149587debb0da01b
Mattia Almansi <m.almansi@bopen.eu>
2024-02-27 11:33:45 +0100

    add legacy client for beta release

---
+++ cads_api_client/legacy_client.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import functools
+import warnings
+from typing import Any, overload
+
+import cdsapi.legacy_client
+
+from . import api_client, processing
+
+
+class LegacyClient(cdsapi.legacy_client.LegacyClient):  # type: ignore[misc]
+    @classmethod
+    def raise_not_implemented_error(self) -> None:
+        raise NotImplementedError(
+            "Thi is a beta version. This functionality has not been implemented yet."
+        )
+
+    @functools.cached_property
+    def client(self) -> api_client.ApiClient:
+        warnings.warn(
+            "This is a beta version."
+            " `url`, `key`, and `session` are the only legacy parameters currently implemented."
+            " Any other parameter is silently ignored.",
+            UserWarning,
+        )
+        return api_client.ApiClient(url=self.url, key=self.key, session=self.session)
+
+    @overload
+    def retrieve(self, name: str, request: dict[str, Any], target: str) -> str:
+        ...
+
+    @overload
+    def retrieve(
+        self, name: str, request: dict[str, Any], target: None
+    ) -> processing.Remote:
+        ...
+
+    def retrieve(
+        self, name: str, request: dict[str, Any], target: str | None = None
+    ) -> str | processing.Remote:
+        collection = self.client.collection(name)
+        remote = collection.submit(**request)
+        return remote if target is None else remote.download(target)
+
+    def service(self, name, *args, **kwargs):  # type: ignore
+        self.raise_not_implemented_error()
+
+    def workflow(self, code, *args, **kwargs):  # type: ignore
+        self.raise_not_implemented_error()
+
+    def status(self, context=None):  # type: ignore
+        self.raise_not_implemented_error()
+
+    def download(self, results, targets=None):  # type: ignore
+        self.raise_not_implemented_error()
+
+    def remote(self, url):  # type: ignore
+        self.raise_not_implemented_error()
+
+    def robust(self, call):  # type: ignore
+        self.raise_not_implemented_error()
