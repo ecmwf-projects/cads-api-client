bee3dd7994e5d18299e415e284e4e2d5b3324037
Mattia Almansi <m.almansi@bopen.eu>
2024-02-29 13:19:49 +0100

    avoid recursion error

--- cads_api_client/legacy_api_client.py
+++ cads_api_client/legacy_api_client.py
@@ -11,6 +11,10 @@ from . import api_client, processing
 
 
 class LegacyApiClient(cdsapi.Client):  # type: ignore[misc]
+    def __new__(cls, *args: Any, **kwargs: Any) -> LegacyApiClient:
+        instantiated: LegacyApiClient = super().__new__(cls)
+        return instantiated
+
     def _initialize_session(self, session: requests.Session) -> requests.Session:
         return session
 
