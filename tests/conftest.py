8ba736f6e4a0d0668565ed49f28f880900200981
Mattia Almansi <m.almansi@bopen.eu>
2024-03-18 16:27:07 +0100

    accept licences in integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,22 @@ def api_root_url() -> str:
 
 
 @pytest.fixture
-def api_key() -> str:
+def api_key(api_root_url: str) -> str:
+    if key := os.getenv("CADS_API_KEY"):
+        return key
+
+    from cads_api_client import ApiClient
+
     # default to test user 1
-    return os.getenv("CADS_API_KEY") or "00000000-0000-4000-a000-000000000000"
+    key = "00000000-0000-4000-a000-000000000000"
+
+    # Accept all licences
+    client = ApiClient(key=key, url=api_root_url)
+    licence_ids = {licence["id"] for licence in client.licences["licences"]}
+    accepted_ids = {licence["id"] for licence in client.accepted_licences["licences"]}
+    for licence_id in licence_ids - accepted_ids:
+        client.accept_licence(licence_id)
+    return key
 
 
 @pytest.fixture
