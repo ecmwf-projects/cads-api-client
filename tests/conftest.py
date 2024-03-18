9e42ecc30a5e5ed77a77fbd8b1d37ab6fb03c8a5
Mattia Almansi <m.almansi@bopen.eu>
2024-03-18 17:18:44 +0100

    requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import os
 from typing import Any
 
 import pytest
+import requests
 
 
 @pytest.fixture
@@ -16,17 +17,14 @@ def api_key(api_root_url: str) -> str:
     if key := os.getenv("CADS_API_KEY"):
         return key
 
-    from cads_api_client import ApiClient
-
     # default to test user 1
     key = "00000000-0000-4000-a000-000000000000"
 
     # Accept all licences
-    client = ApiClient(key=key, url=api_root_url)
-    licence_ids = {licence["id"] for licence in client.licences["licences"]}
-    accepted_ids = {licence["id"] for licence in client.accepted_licences["licences"]}
-    for licence_id in licence_ids - accepted_ids:
-        client.accept_licence(licence_id)
+    result = requests.get(f"{api_root_url}/catalogue/v1/vocabularies/licences")
+    result = requests.patch(
+        api_root_url, json=result.json(), headers={"PRIVATE-TOKEN": key}
+    )
     return key
 
 
