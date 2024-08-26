7b5076ab164a607c0e74dcb366fed5b75e268c88
Mattia Almansi <m.almansi@bopen.eu>
2024-08-26 09:58:02 +0200

    rely on retrieve-api status check

--- cads_api_client/processing.py
+++ cads_api_client/processing.py
@@ -255,9 +255,6 @@ class Remote:
     def make_results(self, url: Optional[str] = None) -> Results:
         if url is None:
             url = self.url
-        status = self.status
-        if status not in ("successful", "failed"):
-            raise ValueError(f"Result not ready, job is {status}")
 
         logger.debug(f"GET {url}")
         request_response = self.session.get(url, headers=self.headers)
@@ -273,7 +270,7 @@ class Remote:
             results_url,
             headers=self.headers,
             session=self.session,
-            raise_for_status=False,
+            raise_for_status=True,
         )
         return results
 
