feaac5bb9b43c1ba8db6d254f051dd030fcd7240
malmans2 <m.almansi@bopen.eu>
2024-10-18 13:30:03 +0200

    restore integrations

--- tests/integration_test_40_results.py
+++ tests/integration_test_40_results.py
@@ -1,4 +1,5 @@
 import contextlib
+import os
 import pathlib
 import random
 from typing import Any
@@ -14,6 +15,24 @@ does_not_raise = contextlib.nullcontext
 @pytest.fixture
 def results(api_anon_client: ApiClient) -> Results:
     return api_anon_client.submit_and_wait_on_results("test-adaptor-dummy", size=1)
+
+
+def test_results_asset(results: Results) -> None:
+    assert results.asset["type"] == "application/x-grib"
+    assert results.asset["file:size"] == 1
+
+
+@pytest.mark.parametrize("target", ("dummy.grib", None))
+def test_results_download(
+    monkeypatch: pytest.MonkeyPatch,
+    results: Results,
+    tmp_path: pathlib.Path,
+    target: str | None,
+) -> None:
+    monkeypatch.chdir(tmp_path)
+    actual_target = results.download(target=target)
+    assert (actual_target != target) if target is None else (actual_target == target)
+    assert os.path.getsize(actual_target) == 1
 
 
 @pytest.mark.parametrize("progress", [True, False])
